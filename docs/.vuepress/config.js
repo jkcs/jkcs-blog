4176549729dc2c549c92b7093fc1382e3ae6d610
jkcs <lwbrave6@gmail.com>
2023-07-26 17:42:39 +0800

    chore: update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,14 @@ module.exports = {
     sidebar: {
       '/': [
         {
+          title: '随记',
+          collapsable: false,
+          children: [
+            '/essay/code-review',
+            '/essay/reflections-on-reverse',
+          ]
+        },
+        {
           title: '逆向与破解',
           collapsable: false,
           children: [
